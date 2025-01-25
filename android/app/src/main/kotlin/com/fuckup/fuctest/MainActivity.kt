9b72363b4bfedf190041b969af6280db67fa5243
JayKosol <kosalaction007@gmail.com>
2025-01-25 17:07:38 +0700

    firts

---
+++ android/app/src/main/kotlin/com/fuckup/fuctest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fuckup.fuctest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MusicPlayerState with ChangeNotifier{
+  AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  void togglePlayback(String audioUrl){
+    if(_isPlaying){
+      _audioPlayer.pause();
+    }
+    else{
+      _audioPlayer.play(UrlSource(audioUrl));
+    }
+    _isPlaying = !_isPlaying;
+    notifyListeners();
+  }
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MusicPlayerState(),
+      child: MaterialApp(
+        title: "MusicPlayer App",
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final musicPlayerState = Provider.of<MusicPlayerState>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Player'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Now Playing: Song Title'),
+            SizedBox(height: 16,),
+            ElevatedButton(
+              onPressed: (){
+                musicPlayerState.togglePlayback('https://www.sample-videos.com/audio/mp3/wave.mp3');
+              },
+              child: musicPlayerState._isPlaying
+                ? Icon(Icons.pause):Icon(Icons.play_arrow)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
